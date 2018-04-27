3820e4a61d17e0422e0ce5e7569d611ce42b8623
rcrios <rcrios@hotmail.com>
2018-04-27 19:25:02 -0300

    Initial commit

---
+++ src/main/java/br/com/rcrios/smartportfolio/SmartPortfolioApplication.java
@@ -0,0 +1,12 @@
+package br.com.rcrios.smartportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPortfolioApplication.class, args);
+	}
+}
